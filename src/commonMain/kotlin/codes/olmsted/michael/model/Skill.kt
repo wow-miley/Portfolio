e2fc49f632f641e82865ac1d00484fed5286bb29
Michael Olmsted <molmsted98@gmail.com>
2022-07-03 13:24:34 -0400

    Add remaining experience items

--- src/commonMain/kotlin/codes/olmsted/michael/model/Skill.kt
+++ src/commonMain/kotlin/codes/olmsted/michael/model/Skill.kt
@@ -2,14 +2,19 @@ package codes.olmsted.michael.model
 
 enum class Skill(val displayName: String) {
     ANDROID("Android"),
+    ARCORE("ARCore"),
     AWS("AWS"),
     FIREBASE("Firebase"),
     GRAPHQL("GraphQL"),
+    HTML("HTML"),
+    IOS("iOS"),
     JAVA("Java"),
     JAVASCRIPT("Javascript"),
     KOTLIN("Kotlin"),
-    SQL("SQL"),
     NODEJS("node.js"),
+    OBJECTIVE_C("Objective-C"),
     REACT("React"),
     REACT_NATIVE("React Native"),
+    SQL("SQL"),
+    UNITY("Unity"),
 }
